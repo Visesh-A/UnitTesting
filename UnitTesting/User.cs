a364397c2f49e4c11039c5dd5d17115bdb366fce
“Visesh-A” <viseshajl@gmail.com>
2023-04-04 21:47:21 +0530

    [visesh]Add:All emails Added

--- UnitTesting/User.cs
+++ UnitTesting/User.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace UnitTesting
+namespace UnitTestings
 {
     public class User
     {
@@ -8,6 +8,7 @@ namespace UnitTesting
         public static string EMAIL = "^[A-Za-z]{3,}[.]{0,}[a-zA-Z]{3,}[@]{1}[a-z]{2,}[.]{1}[a-z]{2,}[.]{1}[a-z]{2,}$";
         public static string MOBILE = "^[0-9]{2}\\s[6-9]{1}[0-9]{9}$";
         public static string PASSWORD = "^[A-Za-z]{4,}[!+@+#+$+%+^+&+*+(+)+_+++~+.+,]{1,}[0-9]{3,}$";
+        public static string ALLEMAIL = "^[a-zA-Z]+[.+_]{1}[a-zA-Z]+[@]{1}[a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$$";
         public string UserFirst(string name)
         {
             if (Regex.IsMatch(name, USER_NAME))
@@ -36,6 +37,16 @@ namespace UnitTesting
         {
             if (Regex.IsMatch(password, PASSWORD))
                 return "Valid";
+            return "InValid";
+        }
+        public string UserAllEmail(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string word = arr[i];
+                if (Regex.IsMatch(word, ALLEMAIL))
+                    return "Valid";
+            }
             return "InValid";
         }
     }
